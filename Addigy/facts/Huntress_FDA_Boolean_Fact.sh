93d5936e9553abc06c7cdd094eb460fec168ae72
percisely-dd <128342373+percisely-dd@users.noreply.github.com>
2025-03-27 16:37:08 -0700

    removed check for app FDA

--- Addigy/facts/Huntress_FDA_Boolean_Fact.sh
+++ Addigy/facts/Huntress_FDA_Boolean_Fact.sh
@@ -1,15 +1,18 @@
 #!/bin/zsh
 #
 # Based on the fact provided by Huntress.  Updated to return a boolean value for easier filtering in Addigy.
+# Removed check for app FDA.
 #
-out=$(sqlite3 -line "/Library/Application Support/com.apple.TCC/TCC.db" "SELECT * FROM access where client == 'com.huntress.app';" | grep service | awk '{print $3}')
-if [[ $out != "kTCCServiceSystemPolicyAllFiles" ]]; then
-    echo "False"
-    exit
-fi
+# out=$(sqlite3 -line "/Library/Application Support/com.apple.TCC/TCC.db" "SELECT * FROM access where client == 'com.huntress.app';" | grep service | awk '{print $3}')
+# if [[ $out != "kTCCServiceSystemPolicyAllFiles" ]]; then
+# # 	echo "first one failed"
+#     echo "False"
+#     exit
+# fi
 
 out=$(sqlite3 -line "/Library/Application Support/com.apple.TCC/TCC.db" "SELECT * FROM access where client == 'com.huntress.sysext';" | grep service | awk '{print $3}')
 if [[ $out != "kTCCServiceSystemPolicyAllFiles" ]]; then
+	echo "second one failed"
     echo "False"
     exit
 fi
